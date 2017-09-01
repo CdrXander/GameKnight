f445e3cac055bc22cf1b31973f6a4bf25b97f0d6
Xander <xackhackman@gmail.com>
2017-08-31 22:40:54 -0600

    Updated syntax to Massive 3.0 Updating db create script. Not finished.

--- db/create_db_script.sql
+++ db/create_db_script.sql
@@ -1,19 +1,32 @@
--- DELETE DATABASE  =   =   =   =   =   =   =   =   =   =   =
-DROP TABLE IF EXISTS public.mechanic_game_link CASCADE;
-DROP TABLE IF EXISTS public.game CASCADE;
-DROP TABLE IF EXISTS public.mechanic CASCADE;
-DROP TABLE IF EXISTS public.genre CASCADE;
+-- DROP TABLES
+DROP TABLE IF EXISTS invite;
+DROP TABLE IF EXISTS game_night;
+DROP TABLE IF EXISTS game_gk_catalog_link;
+DROP TABLE IF EXISTS gk_catalog;
+DROP TABLE IF EXISTS game_genre_link;
+DROP TABLE IF EXISTS game_mechanic_link;
+DROP TABLE IF EXISTS genre;
+DROP TABLE IF EXISTS mechanic;
+DROP TABLE IF EXISTS gk_user;
+DROP TABLE IF EXISTS game;
 
-DROP SEQUENCE IF EXISTS game_pk CASCADE;
-DROP SEQUENCE IF EXISTS mechanic_pk CASCADE;
-DROP SEQUENCE IF EXISTS genre_pk CASCADE;
+-- DROP SEQUENCES
 
+DROP SEQUENCE IF EXISTS game_night_pk;
+DROP SEQUENCE IF EXISTS gk_catalog_pk;
+DROP SEQUENCE IF EXISTS genre_pk;
+DROP SEQUENCE IF EXISTS mechanic_pk;
+DROP SEQUENCE IF EXISTS gk_user_pk;
+DROP SEQUENCE IF EXISTS game_pk;
 
--- CREATE DATABASE  =   =   =   =   =   =   =   =   =   =   =
+-- CREATE SEQUENCES
+CREATE SEQUENCE public.game_pk
+    INCREMENT BY 1
+    START WITH 100
+    MINVALUE 1
+;
 
--- Sequences
-
-CREATE SEQUENCE public.genre_pk
+CREATE SEQUENCE public.gk_user_pk
     INCREMENT BY 1
     START WITH 100
     MINVALUE 1
@@ -25,51 +38,76 @@ CREATE SEQUENCE public.mechanic_pk
     MINVALUE 1
 ;
 
-CREATE SEQUENCE public.game_pk
-    INCREMENT 1
+CREATE SEQUENCE public.genre_pk
+    INCREMENT BY 1
     START WITH 100
     MINVALUE 1
 ;
 
--- Tables
+CREATE SEQUENCE public.gk_catalog_pk
+    INCREMENT BY 1
+    START WITH 100
+    MINVALUE 1
+;
 
-CREATE TABLE public.genre
+CREATE SEQUENCE public.game_night_pk
+    INCREMENT BY 1
+    START WITH 100
+    MINVALUE 1
+;
+
+-- CREATE TABLES
+
+-- BUILD ORDER
+-- game, user, mechanic, genre
+-- game_mechanic_link, game_genre_link
+-- gk_catalog
+-- game_gk_catalog_link
+-- game_night, invite
+
+CREATE TABLE public.game
 (
-    id integer NOT NULL DEFAULT nextval('genre_pk'),
-    title text,
-    description text,
-    PRIMARY KEY (id)
+	id integer NOT NULL DEFAULT nextval('game_pk'),
+	bgg_game_id integer,
+	name text,
+	description text,
+	time_short integer,
+	time_long integer,
+	min_players integer,
+	max_players integer,
+	amazon_link text,
+	image_url text,
+	thumbnail_url text,
+	publish_year integer,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE public.gk_user
+(
+	id integer NOT NULL DEFAULT nextval('gk_user_pk'),
+	fb_id text,
+	display_name text,
+	picture_url text,
+	PRIMARY KEY (id)
 );
 
 CREATE TABLE public.mechanic
 (
-    id integer NOT NULL DEFAULT nextval('mechanic_pk'),
-    title text,
-    description text,
-    PRIMARY KEY (id)
+	id integer NOT NULL DEFAULT nextval('mechanic_pk'),
+	name text,
+	description text,
+	PRIMARY KEY (id)
 );
 
-CREATE TABLE public.game
+CREATE TABLE public.genre
 (
-    id integer NOT NULL DEFAULT nextval('game_pk'),
-    bgg_game_id integer,
-    title text,
-    description text,
-    time_short integer,
-    time_long integer,
-    min_players integer,
-    max_players integer,
-    owned boolean,
-    amazon_link text,
-    image_url text,
-    thumbnail_url text,
-    publish_year integer,
-    genre_id integer,
-    PRIMARY KEY (id),
-    FOREIGN KEY (genre_id) REFERENCES public.genre (id)
+	id integer,
+	name text,
+	description text,
+	PRIMARY KEY (id)
 );
 
-CREATE TABLE public.mechanic_game_link
+CREATE TABLE public.game_mechanic_link
 (
     game_id integer,
     mechanic_id integer,
@@ -78,17 +116,40 @@ CREATE TABLE public.mechanic_game_link
     FOREIGN KEY (mechanic_id) REFERENCES public.mechanic (id)
 );
 
--- TEST DATA
 
--- Genres
-INSERT INTO public.genre VALUES (1,'Strategy', 'Games that require Strategic Thought');
-INSERT INTO public.genre VALUES (2,'Card','Play with overpriced cardboard');
+CREATE TABLE public.game_genre_link
+(
+    game_id integer,
+    genre_id integer,
+    PRIMARY KEY (game_id, genre_id),
+    FOREIGN KEY (game_id) REFERENCES public.game (id),
+    FOREIGN KEY (genre_id) REFERENCES public.genre (id)
+);
 
---  Games
+CREATE TABLE public.gk_catalog
+(
+	id integer NOT NULL DEFAULT nextval('gk_catalog_pk'),
+	name text,
+	description text,
+	owner_id integer,
+	PRIMARY KEY (id),
+	FOREIGN KEY (owner_id) REFERENCES public.gk_user(id)
+);
 
-INSERT INTO public.game VALUES (1,999,'Magic: the Gathering', 'A card game of spells and creatures',20,60,3,5,true,'','','',1993,2);
-INSERT INTO public.game VALUES (2,998,'Chess','The Game of Kings',10,60,2,2,false,'','','',0,1);
-INSERT INTO public.game VALUES (3,997,'Ticket to Ride','Make trains bub',40,60,3,5,true,'','','',2003,1);
+CREATE TABLE public.game_gk_catalog_link
+(
+    game_id integer,
+    gk_catalog_id integer,
+    PRIMARY KEY (game_id, gk_catalog_id),
+    FOREIGN KEY (game_id) REFERENCES public.game (id),
+    FOREIGN KEY (gk_catalog_id) REFERENCES public.gk_catalog (id)
+);
 
-
+CREATE TABLE public.game_night
+(
+	id integer NOT NULL DEFAULT nextval('game_night_pk'),
+	address text,
+	date_time timestamp,
+	PRIMARY KEY (id)
+);
 
